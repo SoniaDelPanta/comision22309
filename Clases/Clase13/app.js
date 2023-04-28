6310f232ce6a6bb01f122f88d497e52e12923c51
soniadelpanta <soniapinci99@gmail.com>
2023-04-28 03:01:10 -0300

    variables

---
+++ Clases/Clase13/app.js
@@ -0,0 +1,50 @@
+
+//primera funcion js
+console.log("hola mundo desde un archivo externo")
+console.log("hola mundo desde un archivo externo 2")
+/*
+DECLARACION DE VARIABLES
+*/
+
+var nombre = prompt("ingrese su nombre", "");
+document.write("el nombre ingresado es ", nombre);
+
+
+let nuemro = 5+10;
+let boolenano = true;
+const pi = 3.1416;
+var x;
+
+var prueba = Number("S");
+console.log(prueba);
+
+
+resultado = nombre+numero;
+var booleano2 = isFinite(numero); //devuelve true o false
+
+
+console.log(numero);
+console.log(nombre);
+console.log(typeof numero);
+console.log(booleano2); 
+console.log(typeof nombre);
+console.log(typeof x);
+console.log(typeof resultado);
+console.log(booleano);
+console.log(pi);
+console.log(x);
+
+
+nombre = 20;
+pi = 25;
+
+console.log(nombre);
+console.log(pi);
+
+
+resultado = nombre*numero;
+console.log("la nueva variable resultado es igual a:", resultado)
+
+
+
+
